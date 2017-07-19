f936df4621d967e94c50bd28063554de7d98562c
szeidman <sam.zeidman@gmail.com>
2017-07-19 10:37:34 -0400

    Add beer model generation

---
+++ db/migrate/20170719143659_create_beers.rb
@@ -0,0 +1,15 @@
+class CreateBeers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :beers do |t|
+      t.string :name
+      t.integer :user_id
+      t.string :style
+      t.float :abv
+      t.float :ibu
+      t.float :srm
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
