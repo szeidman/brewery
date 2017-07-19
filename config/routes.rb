c964f15e3911cfbd0613ca23c2789366e0ee5118
szeidman <sam.zeidman@gmail.com>
2017-07-19 12:13:23 -0400

    Add Ingredients controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,10 @@
 Rails.application.routes.draw do
-  get 'beers/new'
-
-  get 'beers/edit'
-
-  get 'beers/index'
-
-  get 'beers/show'
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'home#index'
   resources :beers
+  resources :items
 
 end
