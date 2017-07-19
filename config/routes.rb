cbeaa424918cba27381d92a58f8c400e9c0d91a5
szeidman <sam.zeidman@gmail.com>
2017-07-19 10:44:38 -0400

    Add beers controller generator and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+  get 'beers/new'
+
+  get 'beers/edit'
+
+  get 'beers/index'
+
+  get 'beers/show'
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'home#index'
-
+  resources :beers
 
 end
