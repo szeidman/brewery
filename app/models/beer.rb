be68c7af5256b0237ffcd87e215dd7e6eb768401
szeidman <sam.zeidman@gmail.com>
2017-07-19 12:20:27 -0400

    Add relations to models

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,5 @@
 class Beer < ApplicationRecord
   belongs_to :user
+  has_many :beer_ingredients
+  has_many :ingredients, through: :beer_ingredients
 end
--- app/models/beer_ingredient.rb
+++ app/models/beer_ingredient.rb
@@ -1,2 +1,4 @@
 class BeerIngredient < ApplicationRecord
+  belongs_to :beer
+  belongs_to :ingredient
 end
