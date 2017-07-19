abc2d6836c39f22f18f0b2f2b01e744cc2e8c789
szeidman <sam.zeidman@gmail.com>
2017-07-19 18:00:43 -0400

    Add color class method to beer, debug partial, begin bitter class method

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -2,4 +2,24 @@ class Beer < ApplicationRecord
   belongs_to :user
   has_many :beer_ingredients
   has_many :ingredients, through: :beer_ingredients
+
+  def color
+    if self.srm < 3.0
+      "Pale Yellow"
+    else
+      "Very Dark"
+    end
+  end
+
+  def bitter
+    if self.ibu > 50
+      "Bitter!"
+    else
+      "Not terribly bitter."
+    end
+  end
+
+  def alcohol
+  end
+
 end
