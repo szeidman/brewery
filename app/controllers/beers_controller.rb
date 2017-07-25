5f38cfcf38d111a6a53cdbb41f4c8d939d2b4036
szeidman <sam.zeidman@gmail.com>
2017-07-24 22:23:10 -0400

    Attempted beer controller debugging

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -7,6 +7,13 @@ class BeersController < ApplicationController
 
   def new
     @beer = Beer.new
+    #["hops", "yeast", "malt", "water"].each do |ingredient_kind|
+    #  @beer.ingredients.build(kind: ingredient_kind)
+    #end
+    @malt = @beer.ingredients.build(kind: "malt")
+    @beer.ingredients.each do |i|
+      i.beer_ingredients.build
+    end
   end
 
   def create
