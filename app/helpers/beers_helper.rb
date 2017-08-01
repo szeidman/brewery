9eb40f7bf15708767815ffae04a1d35b3786926d
szeidman <sam.zeidman@gmail.com>
2017-08-01 17:47:45 -0400

    Revise field_with_error handling for beer form

--- app/helpers/beers_helper.rb
+++ app/helpers/beers_helper.rb
@@ -18,9 +18,17 @@ module BeersHelper
 
   def beer_error?(attribute)
     if @beer.errors[attribute.to_sym].count > 0
-      "field_with_errors"
+        "field_with_errors"
     else
       ""
+    end
+  end
+
+  def base_beer_error(ingredient_kind)
+    if @beer.errors[:base]
+      if @beer.errors.messages[:base].any? {|message| message.include?(ingredient_kind)}
+        "field_with_errors"
+      end
     end
   end
 
