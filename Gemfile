c0c14989bf375a6e7c227538c4882853922920b4
szeidman <sam.zeidman@gmail.com>
2017-10-08 16:39:13 -0400

    Initial handlebars setup and ingredients javascript setup

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'bcrypt', '~> 3.1.7'
 
 gem 'active_model_serializers'
+gem 'handlebars_assets'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
---
+++ app/assets/javascripts/ingredients.js
@@ -0,0 +1,23 @@
+function init() {
+
+}
+document.addEventListener("DOMContentLoaded", function(event) {
+  init()
+})
+
+function Ingredient(attributes) {
+  this.id = attributes.id;
+  this.name = attributes.name;
+  this.kind = attributes.kind;
+  this.origin = attributes.origin;
+  this.beers = [];
+
+  //attributes.beers push each beer
+
+}
+
+function indexList() {
+
+}
+
+// id="ingredients-index-list"
