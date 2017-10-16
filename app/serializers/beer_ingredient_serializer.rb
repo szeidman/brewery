56d199b781c8597a50263e6afddffd9b3322689d
szeidman <sam.zeidman@gmail.com>
2017-10-16 09:59:14 -0400

    Simplify json output in ActiveModel serializers

--- app/serializers/beer_ingredient_serializer.rb
+++ app/serializers/beer_ingredient_serializer.rb
@@ -1,5 +1,5 @@
 class BeerIngredientSerializer < ActiveModel::Serializer
-  attributes :amount, :ingredient_id, :beer_id
+  attributes :amount, :beer_id
   belongs_to :beer
   belongs_to :ingredient
 end
--- app/serializers/beer_serializer.rb
+++ app/serializers/beer_serializer.rb
@@ -1,6 +1,3 @@
 class BeerSerializer < ActiveModel::Serializer
-  attributes :id, :name, :user_id, :style, :abv, :ibu, :srm
-  has_many :ingredients, through: :beer_ingredients
-  has_many :beer_ingredients
-  belongs_to :user
+  attributes :id, :name
 end
